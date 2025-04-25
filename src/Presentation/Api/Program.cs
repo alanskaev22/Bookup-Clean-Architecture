20d25a87d1c7affb555892afd89748a0c2fa0352
alan skaev <alan.skaev22@gmail.com>
2025-04-25 12:07:20 -0400

    Update .editorconfig and clean up Program.cs
    - Updated `.editorconfig` to set analyzer diagnostic severity to 'none' for 'Major Code Smell' and all diagnostics, and added style preferences for C# files.
    - Removed public type warnings and the `Program` class declaration in `Program.cs`, while retaining the integration tests region comment.

--- src/Presentation/Api/Program.cs
+++ src/Presentation/Api/Program.cs
@@ -18,12 +18,7 @@ await app.RunAsync();
 
 #region Needed for integration tests web application factory
 
-#pragma warning disable CA1515 // Consider making public types internal
-#pragma warning disable S1118 // Utility classes should not have public constructors
-
 public partial class Program
-#pragma warning restore S1118 // Utility classes should not have public constructors
-#pragma warning restore CA1515 // Consider making public types internal
 {
 }
 
